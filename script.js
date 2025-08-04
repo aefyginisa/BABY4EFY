ddcd09b5b95b6564731595cb1183df2fa1474a77
Fey <fanne.araza@gmail.com>
2025-08-04 22:10:03 +0800

    updated script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const flap = document.getElementById('flap');
 const paper = document.getElementById('paper');
 const textPage = document.getElementById('textPage');
+const longLetterPage = document.getElementById('longLetterPage');
 const buttons = document.getElementById('buttons');
 const noButton = document.getElementById('noButton');
 
@@ -10,19 +11,14 @@ const slideInSound = document.getElementById('slideInSound');
 const flapCloseSound = document.getElementById('flapCloseSound');
 
 function showPaper() {
-  flap.classList.remove('flap-close');
   flap.classList.add('flap-open');
-
   flapOpenSound.currentTime = 0;
   flapOpenSound.play();
 
   setTimeout(() => {
-    paper.classList.remove('hide');
     paper.classList.add('show');
-
     slideOutSound.currentTime = 0;
     slideOutSound.play();
-
     buttons.style.opacity = '0';
     buttons.style.pointerEvents = 'none';
   }, 800);
@@ -30,21 +26,16 @@ function showPaper() {
 
 function hidePaper() {
   paper.classList.remove('show');
-  paper.classList.add('hide');
-
   textPage.classList.remove('show');
-  textPage.classList.add('hide');
+  longLetterPage.classList.remove('show');
 
   slideInSound.currentTime = 0;
   slideInSound.play();
 
   setTimeout(() => {
     flap.classList.remove('flap-open');
-    flap.classList.add('flap-close');
-
     flapCloseSound.currentTime = 0;
     flapCloseSound.play();
-
     buttons.style.opacity = '1';
     buttons.style.pointerEvents = 'auto';
   }, 800);
@@ -56,35 +47,37 @@ function returnToEnvelope() {
 
 function moveNoButton() {
   const container = document.querySelector('.container');
-  const btn = noButton;
-
-  const maxX = container.clientWidth - btn.offsetWidth;
-  const maxY = container.clientHeight - btn.offsetHeight;
-
+  const maxX = container.clientWidth - noButton.offsetWidth;
+  const maxY = container.clientHeight - noButton.offsetHeight;
   const randomX = Math.random() * maxX;
   const randomY = Math.random() * maxY;
 
-  btn.style.position = 'absolute';
-  btn.style.left = `${randomX}px`;
-  btn.style.top = `${randomY + 200}px`;
+  noButton.style.position = 'absolute';
+  noButton.style.left = `${randomX}px`;
+  noButton.style.top = `${randomY + 200}px`;
 }
 
 function goToTextPage() {
   paper.classList.remove('show');
-  paper.classList.add('hide');
+  longLetterPage.classList.remove('show');
 
   setTimeout(() => {
-    textPage.classList.remove('hide');
     textPage.classList.add('show');
   }, 400);
 }
 
 function goToImagePage() {
   textPage.classList.remove('show');
-  textPage.classList.add('hide');
 
   setTimeout(() => {
-    paper.classList.remove('hide');
     paper.classList.add('show');
+  }, 400);
+}
+
+function goToThirdPage() {
+  textPage.classList.remove('show');
+
+  setTimeout(() => {
+    longLetterPage.classList.add('show');
   }, 400);
 }
